fd0edcba1f0c4b8ba9b814cb42d19f6c093a0c4f
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-26 17:50:21 +0200

    feat: create vue

---
+++ server/server.php
@@ -0,0 +1,16 @@
+<?php
+
+// OBIETTIVO: dato un testo, aggiungere
+// una nuova todo al nostro """database"""
+
+// 1. prendere l'informazione che ci arriva dalla chiamata
+// 2. prendere la lista json delle todo dal """database"""
+// 3. trasformarla in una struttura dati che php può usare (array)
+
+// 4. aggiungere la nuova todo
+
+// 5. trasformare l'array in json (la struttura dati "universale")
+
+// 7. salvare il nuovo json nel nostro """database"""
+
+// 8. restituire il json
